c5afead929887f65c92fa5698a7b16cbd2dbf8db
Bruno Proenca <bruno.proenca@synchro.com.br>
2022-03-11 10:49:32 -0300

    first commit

---
+++ src/main/java/br/com/bcp/websocket/Message.java
@@ -0,0 +1,15 @@
+package br.com.bcp.websocket;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
